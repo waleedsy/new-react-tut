9fe98bc44479fb4d40678b63987cfd576798e0d8
waleedsy <waleedsani2013@gmail.com>
2023-10-11 01:03:48 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const Products = [
+  { title: "Cabbage", id: 1, isFruit: false },
+  { title: "Garlic", id: 2, isFruit: false },
+  { title: "Potato", id: 3, isFruit: false },
+  { title: "Apple", id: 4, isFruit: true },
+];
+
+const [count, setCount] = useState[0];
+
+function handleClick() {
+  alert("You clicked me!");
+  setCount(count + 1);
+}
 
 function App() {
+  const listItems = Products.map((Products) => (
+    <li
+      key={Products.id}
+      style={{
+        color: Products.isFruit ? "magenta" : "darkgreen",
+        textAlign: "left",
+      }}
+    >
+      {Products.title}
+    </li>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to new React tutorial</h1>
+      <ul>{listItems}</ul>
+      <button onClick={handleClick} type="submit">
+        Clicked {count} times
+      </button>
     </div>
   );
 }
